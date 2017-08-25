d9c3549a524cdb716d2440e6d4c50a495f10c6ff
wilxsv <wixl.sv@gmail.com>
2017-08-24 19:18:23 -0600

    despues de limpiar cache

--- siaptb/web/bundles/minsalsiaps/js/AtenAreaModEstab/list.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#reporte').click(function() {
-        url = Routing.generate('_report_show') + '/rCarteraServicios/PDF';
-        window.open(url, '_blank');
-        return false;
-    });
-
-});
-
--- siaptb/web/bundles/minsalsiaps/js/CRUD/base_edit.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $('i').popover('show');       
-});
